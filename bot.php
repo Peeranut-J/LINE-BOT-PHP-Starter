3959a61f7a73276b8d824bb69cf85495265ff089
Peeranut-J <james9007001@hotmail.com>
2018-02-08 22:56:13 +0700

    test mul message

--- bot.php
+++ bot.php
@@ -17,10 +17,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
+			$messages = array(
+			array(
 				'type' => 'text',
 				'text' => $text
-			];
+			),
+			array(
+				'type' => 'text',
+				'text' => 'second message'
+			)
+			);
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
