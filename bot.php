dbbe6c7593e7e72326532b4a56b781ce30c50b76
Peeranut-J <james9007001@hotmail.com>
2018-02-08 22:58:02 +0700

    return bug

--- bot.php
+++ bot.php
@@ -17,16 +17,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = array(
-			array(
+			$messages = [
 				'type' => 'text',
 				'text' => $text
-			),
-			array(
-				'type' => 'text',
-				'text' => 'second message'
-			)
-			);
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
