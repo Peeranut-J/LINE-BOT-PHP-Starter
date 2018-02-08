a024e0ece7a17d7096fb42066ba16b5e12edf749
Peeranut-J <james9007001@hotmail.com>
2018-02-08 23:09:57 +0700

    back to basic

--- bot.php
+++ bot.php
@@ -9,17 +9,17 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format -- test to image
-		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$image = $event['message']['image'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'image',
-				'text' => $image
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,4 +44,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK--test image";
+echo "OK";
