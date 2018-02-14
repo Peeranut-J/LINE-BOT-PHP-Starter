fefb96f635a2ccd0616bdd4433c68bc722def2de
Peeranut-J <james9007001@hotmail.com>
2018-02-14 23:56:08 +0700

    bug fix 03

--- bot.php
+++ bot.php
@@ -9,8 +9,40 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
+			// Get text sent
+			$image = $event['message']['image'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => 'เป็นโรค'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -25,7 +57,7 @@ if (!is_null($events['events'])) {
 			} else if($text == 'ขอรายชื่อโรงพยาบาลที่เกี่ยวข้อง') {
 			$messages = [
 				'type' => 'text',
-				'text' => 'โรงพยาบาล A เบอร์ติดต่อ 02-000-0000 /n โรงพยาบาล B เบอร์ติดต่อ 02-111-1111'
+				'text' => 'โรงพยาบาล A เบอร์ติดต่อ 02-000-0000 \n โรงพยาบาล B เบอร์ติดต่อ 02-111-1111'
 			];
 			} else if($text == 'ปัจจัยเสี่ยงของต้อหิน') {
 			$messages = [
@@ -65,45 +97,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-		else if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-			// Get text sent
-			$image = $event['message']['image'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			if('type' == 'image'){
-			$messages = [
-				'type' => 'text',
-				'text' => 'เป็นโรค'
-			];
-			} else {
-			$messages = [
-				'type' => 'text',
-				'text' => 'หากมีคำถาม หรือต้องการใช้บริการอะไร กรุณากดปุ่มใน bulletin หรือหากต้องการตรวจต้อหินเบื้องต้น กรุณาส่งรูปภาพ ขอบคุณครับ'
-			];
-			}
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
+		
 	}
 }
 echo "OK";
