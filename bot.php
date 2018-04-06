2b0ea281694c49aa48759899d98b0fc68829fe08
Peeranut-J <james9007001@hotmail.com>
2018-04-06 21:26:34 +0700

    huighuh

--- bot.php
+++ bot.php
@@ -74,21 +74,36 @@ if (!is_null($events['events'])) {
 			*/
 		//	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init ($urlIm);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+			//curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			$raw=curl_exec($ch);
+			$img=curl_exec($ch);
 			curl_close ($ch);
-			$fp = fopen($img,'x');
+			/*$fp = fopen($img,'x');
 			fwrite($fp, $raw);
-			fclose($fp);
+			fclose($fp);*/
 
 			if(!empty($img)){
 				$talk = 'yeahhhhhh';
 			} else {
 				$talk = $imageId;
 			}
+/*
+			header("Content-Type: image/jpeg");
+
+			$url = 'https://images.nga.gov/en/web_images/constable.jpg';
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.1 Safari/537.11');
+			$res = curl_exec($ch);
+			$rescode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+			curl_close($ch) ;
+			echo $res;*/
 			/*if(file_exists($saveto)){
 				unlink($saveto);
 			}
