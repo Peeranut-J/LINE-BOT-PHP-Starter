17a5ab2976f26ed0c25cb971054d55e9e533701e
Peeranut-J <james9007001@hotmail.com>
2018-04-12 22:24:44 +0700

    add log

--- bot.php
+++ bot.php
@@ -229,6 +229,8 @@ if (!is_null($events['events'])) {
 				//need to make picture from url .jpg
 				header('Content-Type: image/jpeg');
 				$img = @imagecreatefromjpeg($urlIm);
+				error_log('img00 = ' . $img, 0);
+
 				imagejpeg($img);
 			//	imagedestroy($img);
 
