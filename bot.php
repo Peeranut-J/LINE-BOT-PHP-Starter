fd41be8658256091eb6ee88ff6af933e2af3d3e2
Peeranut-J <james9007001@hotmail.com>
2018-04-13 09:45:43 +0700

    ss

--- bot.php
+++ bot.php
@@ -420,7 +420,7 @@ if (!is_null($events['events'])) {
 
 				// if allowed extension
 				if(in_array($url_info['extension'], $allow)) {
-				  $save_to = 'imgs/'. $url_info['basename'];  // add image with the same name in 'imgs/' folder
+				  $save_to = $url_info['basename'];  // add image with the same name in 'imgs/' folder
 				  if(file_put_contents($save_to, $img)) {
 					$re = '<img src="'.  $save_to .'" title="'. $url_info['basename'] .'" />';
 				  }
